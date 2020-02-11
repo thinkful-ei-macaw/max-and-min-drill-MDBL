919f17cef07e45fec7e67d9b7313f693ce3fca20
mdavis9988 <58867354+mdavis9988@users.noreply.github.com>
2020-02-11 16:40:56 -0500

    added new iterating assignment

---
+++ iteratingoverobjectproperties.js
@@ -0,0 +1,11 @@
+// Create an object with five properties: foo, bar, fum, quux, and spam. Give each property a unique value of your choosing.
+// Loop over the object using for ... in
+// Use console.log to show each property name and its associated value.
+'use strict';
+
+const object = { foo : 1, bar: 3, fum: 5, quux: 7, spam: 9 };
+for (const property in object) {
+    console.log(`${property}: ${object[property]}`);
+  }
+
+
