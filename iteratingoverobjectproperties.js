f4eb6a77559711f068db9e0be41b3b7c44285103
bleek42 <brandonleek42@gmail.com>
2020-02-11 19:15:22 -0500

    Update iteratingoverobjectproperties.js

--- iteratingoverobjectproperties.js
+++ iteratingoverobjectproperties.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 // Create an object with five properties: foo, bar, fum, quux, and spam. Give each property a unique value of your choosing.
 // Loop over the object using for ... in
 // Use console.log to show each property name and its associated value.
@@ -5,7 +6,7 @@
 
 const object = { foo : 1, bar: 3, fum: 5, quux: 7, spam: 9 };
 for (const property in object) {
-    console.log(`${property}: ${object[property]}`);
-  }
+  console.log(`${property}: ${object[property]}`);
+};
 
 
